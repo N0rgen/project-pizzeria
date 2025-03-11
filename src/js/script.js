2f3deee620bb06b2b87eb1a38b1486c68f29466b
Kamil <norgen18@wp.pl>
2025-03-11 21:05:35 +0100

    add accordin function to product list

--- src/js/script.js
+++ src/js/script.js
@@ -90,20 +90,32 @@ const select = {
       const thisProduct = this;
 
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = ???;
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function(event) {
+      if(clickableTrigger) {
+        clickableTrigger.addEventListener('click', function(event) {
 
-      /* prevent default action for event */
+        /* prevent default action for event */
 
-      /* find active product (product that has active class) */
+        event.preventDefault();
 
-      /* if there is active product and it's not thisProduct.element, remove class active from it */
+        /* find active product (product that has active class) */
 
-      /* toggle active class on thisProduct.element */
+        const activeProduct = document.querySelector(select.menuProduct.clickable +'.active');
 
-      });
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+
+        if(activeProduct && activeProduct !== thisProduct.element ){
+          activeProduct.classList.remove('active');
+        }
+
+        /* toggle active class on thisProduct.element */
+
+        thisProduct.element.classList.toggle('active');
+
+        });
+      }
     }
   }
 
