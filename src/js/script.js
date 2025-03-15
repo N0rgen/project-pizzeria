f40c3e47ef8845200312c8b3cd38ac569a477d62
Kamil <norgen18@wp.pl>
2025-03-15 22:47:44 +0100

    further improvments

--- src/js/script.js
+++ src/js/script.js
@@ -180,6 +180,7 @@
       thisProduct.cartButton.addEventListener('click', function(event){
         event.preventDefault();
         thisProduct.processOrder();
+        thisProduct.addToCart();
       });
 
     }
@@ -221,6 +222,8 @@
       }
       // multiply price by amount
       price *= thisProduct.amountWidget.value;
+
+      thisProduct.priceSingle = price;
       
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
@@ -233,6 +236,48 @@
       thisProduct.amountWidgetElem.addEventListener('updated', function(){
         thisProduct.processOrder();
       });
+    }
+
+    addToCart(){
+      const thisProduct = this;
+
+      
+      app.cart.add(thisProduct.prepareCartProduct());
+    }
+
+    prepareCartProduct(){
+      const thisProduct = this;
+      const productSummary = {
+        id: thisProduct.id,
+        name: thisProduct.data.name,
+        amount: thisProduct.amountWidget.value,
+        priceSingle: thisProduct.priceSingle,
+        price: thisProduct.priceSingle * thisProduct.amountWidget.value,
+        params: thisProduct.prepareCartProductParams(),
+      };
+      return productSummary;
+    }
+
+    prepareCartProductParams(){
+      const thisProduct = this;
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      const params = {};
+      for(let paramId in thisProduct.data.params) {
+        const param = thisProduct.data.params[paramId];
+        params[paramId] = {
+          label: param.label,
+          options: {}
+        };
+        for(let optionId in param.options) {
+          const option = param.options[optionId];
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          if(optionSelected){
+            params[paramId].options[optionId] = option.label;
+          }
+        }
+      }
+      return params;
     }
   }
 
@@ -324,6 +369,10 @@
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
       });
     }
+    add(menuProduct){
+      // const thisCart = this;
+      console.log('adding product', menuProduct);
+    }
     
   }
 
@@ -350,8 +399,6 @@
     },
 
     
-    
-
     init: function(){
       const thisApp = this;
       /* console.log('*** App starting ***');
